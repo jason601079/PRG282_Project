30542b6c521064e70d3dbe4afa47c2a0203059fc
Brad Mc Helem <bramch10@gmail.com>
2024-10-28 15:35:46 +0200

    adding database dataset

---
+++ PRG282_Project/DatabaseHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_Project
+{
+    internal class DatabaseHelper
+    {
+        private string _connectionString;
+
+        public DatabaseHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void LoadStudentData(DataGridView dataGridView)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"
+                    SELECT Students.[Student name], Modules.[Module], Modules.Mark
+                    FROM Students
+                    INNER JOIN Modules ON Students.[Student number] = Modules.[Student number]";
+
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
