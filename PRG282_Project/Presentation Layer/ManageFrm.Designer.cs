5c0c1c450e9728eddcfe26db0d64d8c4f1ea0901
Trent Evans <trent.rhett12@icloud.com>
2024-10-29 22:27:54 +0200

    Adding a manage and adding details for the students forms

---
+++ PRG282_Project/Presentation Layer/ManageFrm.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace PRG282_Project.Presentation_Layer
+{
+    partial class ManageFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            guna2DataGridView1 = new Guna.UI2.WinForms.Guna2DataGridView();
+            panel1 = new Panel();
+            label7 = new Label();
+            btn_Update = new Button();
+            btnClear = new Button();
+            btnUpload = new Button();
+            pictureBox1 = new PictureBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            radio_Female = new RadioButton();
+            radio_Male = new RadioButton();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            btn_Delete = new Button();
+            btn_Search = new Button();
+            textBox5 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)guna2DataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2DataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = Color.White;
+            guna2DataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            guna2DataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(100, 88, 255);
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            guna2DataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            guna2DataGridView1.ColumnHeadersHeight = 4;
+            guna2DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.White;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle3.ForeColor = Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(231, 229, 255);
+            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            guna2DataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            guna2DataGridView1.GridColor = Color.FromArgb(231, 229, 255);
+            guna2DataGridView1.Location = new Point(2, 111);
+            guna2DataGridView1.Name = "guna2DataGridView1";
+            guna2DataGridView1.RowHeadersVisible = false;
+            guna2DataGridView1.Size = new Size(866, 226);
+            guna2DataGridView1.TabIndex = 1;
+            guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = Color.White;
+            guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
+            guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.ForeColor = Color.Empty;
+            guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = Color.Empty;
+            guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = Color.Empty;
+            guna2DataGridView1.ThemeStyle.BackColor = SystemColors.Control;
+            guna2DataGridView1.ThemeStyle.GridColor = Color.FromArgb(231, 229, 255);
+            guna2DataGridView1.ThemeStyle.HeaderStyle.BackColor = Color.FromArgb(100, 88, 255);
+            guna2DataGridView1.ThemeStyle.HeaderStyle.BorderStyle = DataGridViewHeaderBorderStyle.None;
+            guna2DataGridView1.ThemeStyle.HeaderStyle.Font = new Font("Segoe UI", 9F);
+            guna2DataGridView1.ThemeStyle.HeaderStyle.ForeColor = Color.White;
+            guna2DataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            guna2DataGridView1.ThemeStyle.HeaderStyle.Height = 4;
+            guna2DataGridView1.ThemeStyle.ReadOnly = false;
+            guna2DataGridView1.ThemeStyle.RowsStyle.BackColor = Color.White;
+            guna2DataGridView1.ThemeStyle.RowsStyle.BorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            guna2DataGridView1.ThemeStyle.RowsStyle.Font = new Font("Segoe UI", 9F);
+            guna2DataGridView1.ThemeStyle.RowsStyle.ForeColor = Color.White;
+            guna2DataGridView1.ThemeStyle.RowsStyle.Height = 25;
+            guna2DataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = Color.FromArgb(231, 229, 255);
+            guna2DataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            guna2DataGridView1.CellContentClick += guna2DataGridView1_CellContentClick;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 71, 160);
+            panel1.Controls.Add(label7);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(868, 54);
+            panel1.TabIndex = 2;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century Gothic", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(269, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(323, 32);
+            label7.TabIndex = 3;
+            label7.Text = "Manage Student details";
+            // 
+            // btn_Update
+            // 
+            btn_Update.BackColor = Color.FromArgb(0, 71, 160);
+            btn_Update.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold);
+            btn_Update.ForeColor = Color.White;
+            btn_Update.Location = new Point(387, 516);
+            btn_Update.Name = "btn_Update";
+            btn_Update.Size = new Size(95, 40);
+            btn_Update.TabIndex = 35;
+            btn_Update.Text = "Update";
+            btn_Update.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = Color.FromArgb(0, 71, 160);
+            btnClear.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold);
+            btnClear.ForeColor = Color.White;
+            btnClear.Location = new Point(250, 516);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(95, 40);
+            btnClear.TabIndex = 34;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnUpload
+            // 
+            btnUpload.BackColor = Color.FromArgb(0, 71, 160);
+            btnUpload.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnUpload.ForeColor = Color.White;
+            btnUpload.Location = new Point(732, 448);
+            btnUpload.Name = "btnUpload";
+            btnUpload.Size = new Size(67, 30);
+            btnUpload.TabIndex = 33;
+            btnUpload.Text = "Upload";
+            btnUpload.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.WhiteSmoke;
+            pictureBox1.Location = new Point(681, 343);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(166, 99);
+            pictureBox1.TabIndex = 32;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(387, 407);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(101, 23);
+            textBox4.TabIndex = 31;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label6.ForeColor = Color.FromArgb(0, 71, 160);
+            label6.Location = new Point(275, 411);
+            label6.Name = "label6";
+            label6.Size = new Size(66, 19);
+            label6.TabIndex = 30;
+            label6.Text = "Course:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(141, 407);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(101, 23);
+            textBox3.TabIndex = 29;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label5.ForeColor = Color.FromArgb(0, 71, 160);
+            label5.Location = new Point(21, 411);
+            label5.Name = "label5";
+            label5.Size = new Size(81, 19);
+            label5.TabIndex = 28;
+            label5.Text = "StudentID";
+            // 
+            // radio_Female
+            // 
+            radio_Female.AutoSize = true;
+            radio_Female.ForeColor = Color.Black;
+            radio_Female.Location = new Point(596, 407);
+            radio_Female.Name = "radio_Female";
+            radio_Female.Size = new Size(63, 19);
+            radio_Female.TabIndex = 27;
+            radio_Female.TabStop = true;
+            radio_Female.Text = "Female";
+            radio_Female.UseVisualStyleBackColor = true;
+            // 
+            // radio_Male
+            // 
+            radio_Male.AutoSize = true;
+            radio_Male.ForeColor = Color.Black;
+            radio_Male.Location = new Point(596, 364);
+            radio_Male.Name = "radio_Male";
+            radio_Male.Size = new Size(51, 19);
+            radio_Male.TabIndex = 26;
+            radio_Male.TabStop = true;
+            radio_Male.Text = "Male";
+            radio_Male.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(0, 71, 160);
+            label4.Location = new Point(509, 362);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 19);
+            label4.TabIndex = 25;
+            label4.Text = "Gender:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(141, 452);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(203, 23);
+            dateTimePicker1.TabIndex = 24;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(0, 71, 160);
+            label3.Location = new Point(21, 454);
+            label3.Name = "label3";
+            label3.Size = new Size(103, 19);
+            label3.TabIndex = 23;
+            label3.Text = "Date of Birth:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(387, 358);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(104, 23);
+            textBox2.TabIndex = 22;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(0, 71, 160);
+            label2.Location = new Point(266, 362);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 19);
+            label2.TabIndex = 21;
+            label2.Text = "Last Name:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(141, 357);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(101, 23);
+            textBox1.TabIndex = 20;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(0, 71, 160);
+            label1.Location = new Point(21, 362);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 19);
+            label1.TabIndex = 19;
+            label1.Text = "First Name:";
+            // 
+            // btn_Delete
+            // 
+            btn_Delete.BackColor = Color.FromArgb(0, 71, 160);
+            btn_Delete.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold);
+            btn_Delete.ForeColor = Color.White;
+            btn_Delete.Location = new Point(527, 516);
+            btn_Delete.Name = "btn_Delete";
+            btn_Delete.Size = new Size(92, 40);
+            btn_Delete.TabIndex = 36;
+            btn_Delete.Text = "Delete";
+            btn_Delete.UseVisualStyleBackColor = false;
+            // 
+            // btn_Search
+            // 
+            btn_Search.BackColor = Color.FromArgb(0, 71, 160);
+            btn_Search.ForeColor = Color.WhiteSmoke;
+            btn_Search.Location = new Point(763, 62);
+            btn_Search.Name = "btn_Search";
+            btn_Search.Size = new Size(87, 37);
+            btn_Search.TabIndex = 38;
+            btn_Search.Text = "Search";
+            btn_Search.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(625, 72);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(130, 23);
+            textBox5.TabIndex = 37;
+            textBox5.Text = "Search by StudentID";
+            // 
+            // ManageFrm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(868, 564);
+            Controls.Add(btn_Search);
+            Controls.Add(textBox5);
+            Controls.Add(btn_Delete);
+            Controls.Add(btn_Update);
+            Controls.Add(btnClear);
+            Controls.Add(btnUpload);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox4);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(radio_Female);
+            Controls.Add(radio_Male);
+            Controls.Add(label4);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Controls.Add(guna2DataGridView1);
+            Name = "ManageFrm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ManageFrm";
+            ((System.ComponentModel.ISupportInitialize)guna2DataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2DataGridView guna2DataGridView1;
+        private Panel panel1;
+        private Label label7;
+        private Button btn_Update;
+        private Button btnClear;
+        private Button btnUpload;
+        private PictureBox pictureBox1;
+        private TextBox textBox4;
+        private Label label6;
+        private TextBox textBox3;
+        private Label label5;
+        private RadioButton radio_Female;
+        private RadioButton radio_Male;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button btn_Delete;
+        private Button btn_Search;
+        private TextBox textBox5;
+    }
+}
