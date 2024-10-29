436380b66b636572a58eec0c95a2f6e8c8bbbd62
Trent Evans <trent.rhett12@icloud.com>
2024-10-29 11:23:08 +0200

    Admin Home Page

---
+++ PRG282_Project/Presentation Layer/Home.Designer.cs
@@ -0,0 +1,442 @@
+﻿namespace PRG282_Project.Presentation_Layer
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Panel_Home = new Panel();
+            Btn_Exit = new Button();
+            Panel_Score = new Panel();
+            Btn_PrntScore = new Button();
+            Btn_ManageScore = new Button();
+            Btn_NewScore = new Button();
+            Btn_Score = new Button();
+            Panel_Course = new Panel();
+            Btn_PrintCourse = new Button();
+            Btn_ManageCourse = new Button();
+            Btn_NewCOurse = new Button();
+            Btn_Course = new Button();
+            panel_Student = new Panel();
+            Btn_Prnt_Std = new Button();
+            Btn_Status = new Button();
+            btn_Mng = new Button();
+            Btn_reg = new Button();
+            Btn_Student = new Button();
+            Panel_logo = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            Panel_Home.SuspendLayout();
+            Panel_Score.SuspendLayout();
+            Panel_Course.SuspendLayout();
+            panel_Student.SuspendLayout();
+            Panel_logo.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Panel_Home
+            // 
+            Panel_Home.AutoScroll = true;
+            Panel_Home.BackColor = Color.FromArgb(0, 71, 160);
+            Panel_Home.Controls.Add(Btn_Exit);
+            Panel_Home.Controls.Add(Panel_Score);
+            Panel_Home.Controls.Add(Btn_Score);
+            Panel_Home.Controls.Add(Panel_Course);
+            Panel_Home.Controls.Add(Btn_Course);
+            Panel_Home.Controls.Add(panel_Student);
+            Panel_Home.Controls.Add(Btn_Student);
+            Panel_Home.Controls.Add(Panel_logo);
+            Panel_Home.Dock = DockStyle.Left;
+            Panel_Home.Location = new Point(0, 0);
+            Panel_Home.Name = "Panel_Home";
+            Panel_Home.Size = new Size(198, 596);
+            Panel_Home.TabIndex = 0;
+            // 
+            // Btn_Exit
+            // 
+            Btn_Exit.Dock = DockStyle.Top;
+            Btn_Exit.FlatAppearance.BorderSize = 0;
+            Btn_Exit.FlatStyle = FlatStyle.Flat;
+            Btn_Exit.Font = new Font("Century Gothic", 12F);
+            Btn_Exit.ForeColor = Color.White;
+            Btn_Exit.Location = new Point(0, 722);
+            Btn_Exit.Name = "Btn_Exit";
+            Btn_Exit.Padding = new Padding(10, 0, 0, 0);
+            Btn_Exit.Size = new Size(181, 63);
+            Btn_Exit.TabIndex = 7;
+            Btn_Exit.Text = "Exit";
+            Btn_Exit.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_Exit.UseVisualStyleBackColor = true;
+            // 
+            // Panel_Score
+            // 
+            Panel_Score.BackColor = Color.FromArgb(0, 71, 200);
+            Panel_Score.Controls.Add(Btn_PrntScore);
+            Panel_Score.Controls.Add(Btn_ManageScore);
+            Panel_Score.Controls.Add(Btn_NewScore);
+            Panel_Score.Dock = DockStyle.Top;
+            Panel_Score.Location = new Point(0, 600);
+            Panel_Score.Name = "Panel_Score";
+            Panel_Score.Size = new Size(181, 122);
+            Panel_Score.TabIndex = 6;
+            // 
+            // Btn_PrntScore
+            // 
+            Btn_PrntScore.Dock = DockStyle.Top;
+            Btn_PrntScore.FlatAppearance.BorderSize = 0;
+            Btn_PrntScore.FlatStyle = FlatStyle.Flat;
+            Btn_PrntScore.Font = new Font("Century Gothic", 12F);
+            Btn_PrntScore.ForeColor = Color.White;
+            Btn_PrntScore.Location = new Point(0, 80);
+            Btn_PrntScore.Name = "Btn_PrntScore";
+            Btn_PrntScore.Padding = new Padding(35, 0, 0, 0);
+            Btn_PrntScore.Size = new Size(181, 40);
+            Btn_PrntScore.TabIndex = 3;
+            Btn_PrntScore.Text = "Print";
+            Btn_PrntScore.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_PrntScore.UseVisualStyleBackColor = true;
+            Btn_PrntScore.Click += Btn_PrntScore_Click;
+            // 
+            // Btn_ManageScore
+            // 
+            Btn_ManageScore.Dock = DockStyle.Top;
+            Btn_ManageScore.FlatAppearance.BorderSize = 0;
+            Btn_ManageScore.FlatStyle = FlatStyle.Flat;
+            Btn_ManageScore.Font = new Font("Century Gothic", 12F);
+            Btn_ManageScore.ForeColor = Color.White;
+            Btn_ManageScore.Location = new Point(0, 40);
+            Btn_ManageScore.Name = "Btn_ManageScore";
+            Btn_ManageScore.Padding = new Padding(35, 0, 0, 0);
+            Btn_ManageScore.Size = new Size(181, 40);
+            Btn_ManageScore.TabIndex = 1;
+            Btn_ManageScore.Text = "Manage Score";
+            Btn_ManageScore.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_ManageScore.UseVisualStyleBackColor = true;
+            Btn_ManageScore.Click += Btn_ManageScore_Click;
+            // 
+            // Btn_NewScore
+            // 
+            Btn_NewScore.Dock = DockStyle.Top;
+            Btn_NewScore.FlatAppearance.BorderSize = 0;
+            Btn_NewScore.FlatStyle = FlatStyle.Flat;
+            Btn_NewScore.Font = new Font("Century Gothic", 12F);
+            Btn_NewScore.ForeColor = Color.White;
+            Btn_NewScore.Location = new Point(0, 0);
+            Btn_NewScore.Name = "Btn_NewScore";
+            Btn_NewScore.Padding = new Padding(35, 0, 0, 0);
+            Btn_NewScore.Size = new Size(181, 40);
+            Btn_NewScore.TabIndex = 0;
+            Btn_NewScore.Text = "New Score";
+            Btn_NewScore.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_NewScore.UseVisualStyleBackColor = true;
+            Btn_NewScore.Click += Btn_NewScore_Click;
+            // 
+            // Btn_Score
+            // 
+            Btn_Score.Dock = DockStyle.Top;
+            Btn_Score.FlatAppearance.BorderSize = 0;
+            Btn_Score.FlatStyle = FlatStyle.Flat;
+            Btn_Score.Font = new Font("Century Gothic", 12F);
+            Btn_Score.ForeColor = Color.White;
+            Btn_Score.Location = new Point(0, 537);
+            Btn_Score.Name = "Btn_Score";
+            Btn_Score.Padding = new Padding(10, 0, 0, 0);
+            Btn_Score.Size = new Size(181, 63);
+            Btn_Score.TabIndex = 5;
+            Btn_Score.Text = "Score";
+            Btn_Score.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_Score.UseVisualStyleBackColor = true;
+            Btn_Score.Click += Btn_Score_Click;
+            // 
+            // Panel_Course
+            // 
+            Panel_Course.BackColor = Color.FromArgb(0, 71, 200);
+            Panel_Course.Controls.Add(Btn_PrintCourse);
+            Panel_Course.Controls.Add(Btn_ManageCourse);
+            Panel_Course.Controls.Add(Btn_NewCOurse);
+            Panel_Course.Dock = DockStyle.Top;
+            Panel_Course.Location = new Point(0, 413);
+            Panel_Course.Name = "Panel_Course";
+            Panel_Course.Size = new Size(181, 124);
+            Panel_Course.TabIndex = 4;
+            Panel_Course.Paint += panel1_Paint;
+            // 
+            // Btn_PrintCourse
+            // 
+            Btn_PrintCourse.Dock = DockStyle.Top;
+            Btn_PrintCourse.FlatAppearance.BorderSize = 0;
+            Btn_PrintCourse.FlatStyle = FlatStyle.Flat;
+            Btn_PrintCourse.Font = new Font("Century Gothic", 12F);
+            Btn_PrintCourse.ForeColor = Color.White;
+            Btn_PrintCourse.Location = new Point(0, 80);
+            Btn_PrintCourse.Name = "Btn_PrintCourse";
+            Btn_PrintCourse.Padding = new Padding(35, 0, 0, 0);
+            Btn_PrintCourse.Size = new Size(181, 40);
+            Btn_PrintCourse.TabIndex = 3;
+            Btn_PrintCourse.Text = "Print";
+            Btn_PrintCourse.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_PrintCourse.UseVisualStyleBackColor = true;
+            Btn_PrintCourse.Click += Btn_PrintCourse_Click;
+            // 
+            // Btn_ManageCourse
+            // 
+            Btn_ManageCourse.Dock = DockStyle.Top;
+            Btn_ManageCourse.FlatAppearance.BorderSize = 0;
+            Btn_ManageCourse.FlatStyle = FlatStyle.Flat;
+            Btn_ManageCourse.Font = new Font("Century Gothic", 12F);
+            Btn_ManageCourse.ForeColor = Color.White;
+            Btn_ManageCourse.Location = new Point(0, 40);
+            Btn_ManageCourse.Name = "Btn_ManageCourse";
+            Btn_ManageCourse.Padding = new Padding(35, 0, 0, 0);
+            Btn_ManageCourse.Size = new Size(181, 40);
+            Btn_ManageCourse.TabIndex = 1;
+            Btn_ManageCourse.Text = "Manage Course";
+            Btn_ManageCourse.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_ManageCourse.UseVisualStyleBackColor = true;
+            Btn_ManageCourse.Click += Btn_ManageCourse_Click;
+            // 
+            // Btn_NewCOurse
+            // 
+            Btn_NewCOurse.Dock = DockStyle.Top;
+            Btn_NewCOurse.FlatAppearance.BorderSize = 0;
+            Btn_NewCOurse.FlatStyle = FlatStyle.Flat;
+            Btn_NewCOurse.Font = new Font("Century Gothic", 12F);
+            Btn_NewCOurse.ForeColor = Color.White;
+            Btn_NewCOurse.Location = new Point(0, 0);
+            Btn_NewCOurse.Name = "Btn_NewCOurse";
+            Btn_NewCOurse.Padding = new Padding(35, 0, 0, 0);
+            Btn_NewCOurse.Size = new Size(181, 40);
+            Btn_NewCOurse.TabIndex = 0;
+            Btn_NewCOurse.Text = "New Course";
+            Btn_NewCOurse.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_NewCOurse.UseVisualStyleBackColor = true;
+            Btn_NewCOurse.Click += Btn_NewCOurse_Click;
+            // 
+            // Btn_Course
+            // 
+            Btn_Course.Dock = DockStyle.Top;
+            Btn_Course.FlatAppearance.BorderSize = 0;
+            Btn_Course.FlatStyle = FlatStyle.Flat;
+            Btn_Course.Font = new Font("Century Gothic", 12F);
+            Btn_Course.ForeColor = Color.White;
+            Btn_Course.Location = new Point(0, 350);
+            Btn_Course.Name = "Btn_Course";
+            Btn_Course.Padding = new Padding(10, 0, 0, 0);
+            Btn_Course.Size = new Size(181, 63);
+            Btn_Course.TabIndex = 3;
+            Btn_Course.Text = "Course";
+            Btn_Course.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_Course.UseVisualStyleBackColor = true;
+            Btn_Course.Click += Btn_Course_Click;
+            // 
+            // panel_Student
+            // 
+            panel_Student.BackColor = Color.FromArgb(0, 71, 200);
+            panel_Student.Controls.Add(Btn_Prnt_Std);
+            panel_Student.Controls.Add(Btn_Status);
+            panel_Student.Controls.Add(btn_Mng);
+            panel_Student.Controls.Add(Btn_reg);
+            panel_Student.Dock = DockStyle.Top;
+            panel_Student.Location = new Point(0, 186);
+            panel_Student.Name = "panel_Student";
+            panel_Student.Size = new Size(181, 164);
+            panel_Student.TabIndex = 2;
+            // 
+            // Btn_Prnt_Std
+            // 
+            Btn_Prnt_Std.Dock = DockStyle.Top;
+            Btn_Prnt_Std.FlatAppearance.BorderSize = 0;
+            Btn_Prnt_Std.FlatStyle = FlatStyle.Flat;
+            Btn_Prnt_Std.Font = new Font("Century Gothic", 12F);
+            Btn_Prnt_Std.ForeColor = Color.White;
+            Btn_Prnt_Std.Location = new Point(0, 120);
+            Btn_Prnt_Std.Name = "Btn_Prnt_Std";
+            Btn_Prnt_Std.Padding = new Padding(35, 0, 0, 0);
+            Btn_Prnt_Std.Size = new Size(181, 40);
+            Btn_Prnt_Std.TabIndex = 3;
+            Btn_Prnt_Std.Text = "Print";
+            Btn_Prnt_Std.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_Prnt_Std.UseVisualStyleBackColor = true;
+            Btn_Prnt_Std.Click += Btn_Prnt_Std_Click;
+            // 
+            // Btn_Status
+            // 
+            Btn_Status.Dock = DockStyle.Top;
+            Btn_Status.FlatAppearance.BorderSize = 0;
+            Btn_Status.FlatStyle = FlatStyle.Flat;
+            Btn_Status.Font = new Font("Century Gothic", 12F);
+            Btn_Status.ForeColor = Color.White;
+            Btn_Status.Location = new Point(0, 80);
+            Btn_Status.Name = "Btn_Status";
+            Btn_Status.Padding = new Padding(35, 0, 0, 0);
+            Btn_Status.Size = new Size(181, 40);
+            Btn_Status.TabIndex = 2;
+            Btn_Status.Text = "Status";
+            Btn_Status.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_Status.UseVisualStyleBackColor = true;
+            Btn_Status.Click += Btn_Status_Click;
+            // 
+            // btn_Mng
+            // 
+            btn_Mng.Dock = DockStyle.Top;
+            btn_Mng.FlatAppearance.BorderSize = 0;
+            btn_Mng.FlatStyle = FlatStyle.Flat;
+            btn_Mng.Font = new Font("Century Gothic", 12F);
+            btn_Mng.ForeColor = Color.White;
+            btn_Mng.Location = new Point(0, 40);
+            btn_Mng.Name = "btn_Mng";
+            btn_Mng.Padding = new Padding(35, 0, 0, 0);
+            btn_Mng.Size = new Size(181, 40);
+            btn_Mng.TabIndex = 1;
+            btn_Mng.Text = "Manage Student";
+            btn_Mng.TextAlign = ContentAlignment.MiddleLeft;
+            btn_Mng.UseVisualStyleBackColor = true;
+            btn_Mng.Click += btn_Mng_Click;
+            // 
+            // Btn_reg
+            // 
+            Btn_reg.Dock = DockStyle.Top;
+            Btn_reg.FlatAppearance.BorderSize = 0;
+            Btn_reg.FlatStyle = FlatStyle.Flat;
+            Btn_reg.Font = new Font("Century Gothic", 12F);
+            Btn_reg.ForeColor = Color.White;
+            Btn_reg.Location = new Point(0, 0);
+            Btn_reg.Name = "Btn_reg";
+            Btn_reg.Padding = new Padding(35, 0, 0, 0);
+            Btn_reg.Size = new Size(181, 40);
+            Btn_reg.TabIndex = 0;
+            Btn_reg.Text = "Registration";
+            Btn_reg.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_reg.UseVisualStyleBackColor = true;
+            Btn_reg.Click += Btn_reg_Click;
+            // 
+            // Btn_Student
+            // 
+            Btn_Student.Dock = DockStyle.Top;
+            Btn_Student.FlatAppearance.BorderSize = 0;
+            Btn_Student.FlatStyle = FlatStyle.Flat;
+            Btn_Student.Font = new Font("Century Gothic", 12F);
+            Btn_Student.ForeColor = Color.White;
+            Btn_Student.Location = new Point(0, 123);
+            Btn_Student.Name = "Btn_Student";
+            Btn_Student.Padding = new Padding(10, 0, 0, 0);
+            Btn_Student.Size = new Size(181, 63);
+            Btn_Student.TabIndex = 1;
+            Btn_Student.Text = "Student";
+            Btn_Student.TextAlign = ContentAlignment.MiddleLeft;
+            Btn_Student.UseVisualStyleBackColor = true;
+            Btn_Student.Click += Btn_Student_Click;
+            // 
+            // Panel_logo
+            // 
+            Panel_logo.Controls.Add(label3);
+            Panel_logo.Controls.Add(label2);
+            Panel_logo.Controls.Add(label1);
+            Panel_logo.Dock = DockStyle.Top;
+            Panel_logo.Location = new Point(0, 0);
+            Panel_logo.Name = "Panel_logo";
+            Panel_logo.Size = new Size(181, 123);
+            Panel_logo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Broadway", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(41, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 55);
+            label1.TabIndex = 0;
+            label1.Text = "BC";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(3, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 19);
+            label2.TabIndex = 1;
+            label2.Text = "System";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(1, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(177, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Student Management";
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1055, 596);
+            Controls.Add(Panel_Home);
+            Name = "Home";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Home Page";
+            Load += Home_Load;
+            Panel_Home.ResumeLayout(false);
+            Panel_Score.ResumeLayout(false);
+            Panel_Course.ResumeLayout(false);
+            panel_Student.ResumeLayout(false);
+            Panel_logo.ResumeLayout(false);
+            Panel_logo.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel Panel_Home;
+        private Panel panel_Student;
+        private Button Btn_Student;
+        private Panel Panel_logo;
+        private Button Btn_Prnt_Std;
+        private Button Btn_Status;
+        private Button btn_Mng;
+        private Button Btn_reg;
+        private Button Btn_Exit;
+        private Panel Panel_Score;
+        private Button Btn_PrntScore;
+        private Button Btn_ManageScore;
+        private Button Btn_NewScore;
+        private Button Btn_Score;
+        private Panel Panel_Course;
+        private Button Btn_PrintCourse;
+        private Button Btn_ManageCourse;
+        private Button Btn_NewCOurse;
+        private Button Btn_Course;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+    }
+}
